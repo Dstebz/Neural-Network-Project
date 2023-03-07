ed40fafd4f265c805aec75bab8d886eb39661e12
Dstebz <daniel.stebbings.2021@uni.strath.ac.uk>
2023-03-07 13:11:47 +0000

    commitiing activation function

---
+++ Neural Network Project/ActivationFunction.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// f(x) = max(0,x)
+
+
+class ActivationFunction {
+public:
+	
+	static double reLU(double ip);			//maybe include this within layers since it's not taking up much space
+	static double tanh(double ip);
+	static double sigmoid(double ip);	
+	static double softmax(double ip);
+};
--- Neural Network Project/ReLU.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// f(x) = max(0,x)
-
-double reLU(double ip);			//maybe include this within layers since it's not taking up much space
-
