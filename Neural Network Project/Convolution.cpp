fe48de6d35630f56238944b40df86a6b8c87e74a
Dstebz <daniel.stebbings.2021@uni.strath.ac.uk>
2023-04-25 11:32:25 +0100

    Improving comments

--- Neural Network Project/Convolution.cpp
+++ Neural Network Project/Convolution.cpp
@@ -37,20 +37,21 @@ Eigen::MatrixXd ConvolutionLayer::Run(Eigen::MatrixXd input) {
 	Eigen::MatrixXd output = Eigen::MatrixXd::Zero(outputDimensionX, outputDimensionY);
 
 	//adding padding to input
-	Eigen::MatrixXd paddedinput = Eigen::MatrixXd::Zero(input.rows() + 2 * this->parameters.padding,
-		input.cols() + 2 * this->parameters.padding);
-	paddedinput.block(this->parameters.padding,
+	Eigen::MatrixXd paddedinput = Eigen::MatrixXd::Zero(input.rows() + 2 * this->parameters.padding, //add padding to rows
+		input.cols() + 2 * this->parameters.padding); //add padding to columns
+	paddedinput.block(this->parameters.padding, //start at padding, padding
 		this->parameters.padding,
-		input.rows(),
-		input.cols()) = input;
+		input.rows(), //take input row dimension
+		input.cols() //take input column dimension
+	) = input; //assign input into the padded matrix
 
 
 	for (int i = 0; i < outputDimensionX; i++) {
 		for (int j = 0; j < outputDimensionY; j++) {
-			output(i, j) = (paddedinput.block(i * this->parameters.stride,
+			output(i, j) = (paddedinput.block(i * this->parameters.stride, //start at i*stride, j*stride (top left corner of kernel)
 				j * this->parameters.stride,
 				this->parameters.kernelSize,
-				this->parameters.kernelSize) //start at i,j, take kernelSize x kernelSize block
+				this->parameters.kernelSize) //take kernelSize x kernelSize block
 				.cwiseProduct(this->kernel)).sum(); //get sum of elementwise product
 		}
 	}
